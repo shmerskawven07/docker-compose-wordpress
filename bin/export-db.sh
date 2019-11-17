b6d12c3f9e009e15c1eaa044af71f64fd039defc
amineo <amineo@macbook-air-de-amine.home>
2019-11-17 21:16:03 +0100

    new docker compose. http ok

--- bin/export-db.sh
+++ bin/export-db.sh
@@ -5,7 +5,7 @@ file="$this_dir/../data/dump.sql"
  
 # Create dump file
 cmd='exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_ROOT_PASSWORD"'
-docker-compose exec db sh -c "$cmd" > $file
+docker-compose exec wordpress-db sh -c "$cmd" > $file
  
 # Remove password warning from the file
 sed -i '.bak' 1,1d $file && rm "$file.bak"
